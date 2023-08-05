f7519120691e653f3fe72fbaec25a121fd1286ea
Kevin Ferati <kevin.ferati@hes-so.ch>
2023-08-05 08:50:07 +0200

    Test pour le login basique

--- GameSync.Api.Tests/SIgnUpTests.cs
+++ GameSync.Api.Tests/SIgnUpTests.cs
@@ -1,90 +1,86 @@
 ﻿using Bogus.DataSets;
 using GameSync.Api.Endpoints.Users;
-using GameSync.Business.Features.Search;
 using Microsoft.AspNetCore.Identity;
 using System.Net;
 using System.Net.Http.Json;
 using Xunit;
 using Xunit.Abstractions;
 
-namespace GameSync.Api.Tests
+namespace GameSync.Api.Tests;
+
+public class SignUpTests : IClassFixture<GameSyncAppFactory>
 {
-    public class SIgnUpTests : IClassFixture<GameSyncAppFactory>
+    private readonly GameSyncAppFactory _factory;
+
+    public SignUpTests(GameSyncAppFactory integrationTestFactory)
     {
-        private readonly GameSyncAppFactory _factory;
-        private readonly ITestOutputHelper _output;
+        _factory = integrationTestFactory;
+    }
 
-        public SIgnUpTests(GameSyncAppFactory integrationTestFactory, ITestOutputHelper output)
-        {
-            _factory = integrationTestFactory;
-            _output = output;
-        }
+    [Fact]
+    public async Task SecurePassword_ProduceNoError()
+    {
+        // arrange
+        var testRequest = GetTestRequest("$UX#%A!qaphEL2");
 
-        [Fact]
-        public async Task SecurePassword_ProduceNoError()
-        {
-            // arrange
-            var testRequest = GetTestRequest("$UX#%A!qaphEL2");
+        // act
+        var response = await TryCreateAccount(testRequest);
+        
+        // assert
+        var payload = await response.Content.ReadFromJsonAsync<SignUpValidResponse>();
+        Assert.NotNull(payload);
+        Assert.Equal(testRequest.Email, payload.Email);
 
-            // act
-            var response = await TryCreateAccout(testRequest);
-            
-            // assert
-            var payload = await response.Content.ReadFromJsonAsync<SignUpValidResponse>();
-            Assert.NotNull(payload);
-            Assert.Equal(testRequest.Email, payload.Email);
-
-        }
- 
-
-        [Theory]
-        [InlineData("asfWJIj1421", "PasswordRequiresNonAlphanumeric")]
-        [InlineData("$1241245ds125", "PasswordRequiresUpper")]
-        [InlineData("$XaBaBweqgRw", "PasswordRequiresDigit")]
-        [InlineData("$X3%A!q", "PasswordTooShort")]
-        public async Task BadlyFormedPassword(string password, string expectedError)
-        {
-            // arrange
-            var testRequest = GetTestRequest(password);
-
-            // act
-            var response = await TryCreateAccout(testRequest);
-
-            // assert
-            await AssertProduceError(expectedError, response);
-        }
-
-        [Fact]
-        public async Task BadlyFormedMail()
-        {
-            var response = await TryCreateAccout(new SignUpRequest { Email = "ab", Password = "$UX#%A!qaphEL2a23" });
-
-            await AssertProduceError("InvalidEmail", response);
-        }
+    }
 
 
-        private static SignUpRequest GetTestRequest(string password) => new SignUpRequest
-        {
-            Email = new Internet().Email(),
-            Password = password
-        };
+    [Theory]
+    [InlineData("asfWJIj1421", "PasswordRequiresNonAlphanumeric")]
+    [InlineData("$1241245ds125", "PasswordRequiresUpper")]
+    [InlineData("$XaBaBweqgRw", "PasswordRequiresDigit")]
+    [InlineData("$X3%A!q", "PasswordTooShort")]
+    public async Task BadlyFormedPassword(string password, string expectedError)
+    {
+        // arrange
+        var testRequest = GetTestRequest(password);
 
-        private async Task<HttpResponseMessage> TryCreateAccout(SignUpRequest req)
-        {
-            var client = _factory.CreateClient();
-            return await client.PostAsJsonAsync("/api/users/sign-in", req);
-        }
+        // act
+        var response = await TryCreateAccount(testRequest);
 
-        private async Task AssertProduceError(string errorCode, HttpResponseMessage response)
-        {
-            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
-            var typedResponse = await response.Content.ReadFromJsonAsync<IEnumerable<IdentityError>>();
-            Assert.NotNull(typedResponse);
-            var error = Assert.Single(typedResponse);
-            Assert.Equal(errorCode, error.Code);
-        }
+        // assert
+        await AssertProduceError(expectedError, response);
+    }
+
+    [Fact]
+    public async Task BadlyFormedMail()
+    {
+        var badlyFormedMailSignInRequest = new SignUpRequest { Email = "ab", Password = "$UX#%A!qaphEL2a23" };
+
+        var response = await TryCreateAccount(badlyFormedMailSignInRequest);
+
+        await AssertProduceError("InvalidEmail", response);
+    }
 
 
+    private static SignUpRequest GetTestRequest(string password) => new SignUpRequest
+    {
+        Email = new Internet().Email(),
+        Password = password
+    };
+
+    private async Task<HttpResponseMessage> TryCreateAccount(SignUpRequest req)
+    {
+        var client = _factory.CreateClient();
+        return await client.PostAsJsonAsync("/api/users/sign-in", req);
+    }
+
+    private async Task AssertProduceError(string errorCode, HttpResponseMessage response)
+    {
+        Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+        var typedResponse = await response.Content.ReadFromJsonAsync<IEnumerable<IdentityError>>();
+        Assert.NotNull(typedResponse);
+        var error = Assert.Single(typedResponse);
+        Assert.Equal(errorCode, error.Code);
     }
 
 

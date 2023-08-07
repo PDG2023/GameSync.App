05d84684aa675e039530fcf2b09103dd130b9e0b
Kevin Ferati <kevin.ferati@hes-so.ch>
2023-08-07 15:22:53 +0200

    Service de messagerie OK (secrets cachés)

--- GameSync.Business/Auth/Mailing/AzureAuthMailService.cs
+++ GameSync.Business/Auth/Mailing/AzureAuthMailService.cs
@@ -1,10 +1,42 @@
-﻿namespace GameSync.Business.Auth.Mailing
+﻿using Azure;
+using Azure.Communication.Email;
+
+namespace GameSync.Business.Auth.Mailing
 {
     public class AzureAuthMailService : IAuthMailService
     {
-        public Task<bool> SendEmailConfirmationAsync(string toEmail, string mailConfirmationToken)
-        {
 
+        public async Task<bool> SendEmailConfirmationAsync(string toEmail, string mailConfirmationToken)
+        {
+            // This code demonstrates how to send email using Azure Communication Services.
+            var emailClient = new EmailClient(connectionString);
+            var message = BuildEmail(sender, toEmail, mailConfirmationToken);
+
+            try
+            {
+
+                var emailSendOperation = await emailClient.SendAsync(Azure.WaitUntil.Completed, message);
+                var sendResult = emailSendOperation.Value;
+                return true;
+            }
+            catch (RequestFailedException e)
+            {
+                Console.WriteLine($"Email send operation failed with error code: {e.ErrorCode}, message: {e.Message}");
+                return false;
+            }
+
+        }
+
+        private static EmailMessage BuildEmail(string senderMail, string recipientMail, string confirmationToken)
+        {
+            var link = new ConfirmationMailLinkProvider().GetConfirmationMailLink(recipientMail, confirmationToken, "http", "localhost");
+            var content = new EmailContent("Confirm your registration")
+            {
+                Html =  $"<a href=\"{link}\">Confirm your mail</a>"
+            };
+            return new EmailMessage(senderMail, recipientMail, content);
         }
     }
 }
+
+           

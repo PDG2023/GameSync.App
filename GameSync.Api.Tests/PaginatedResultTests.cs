630a06d1ee77856031c1871142e150b92c36d67d
Kevin Ferati <kevin.ferati@hes-so.ch>
2023-08-15 19:30:20 +0200

    PaginatedResult : Handling page number and page sizes and the generation of the url to the NextPage

--- GameSync.Api.Tests/PaginatedResultTests.cs
+++ GameSync.Api.Tests/PaginatedResultTests.cs
@@ -4,13 +4,34 @@ namespace GameSync.Api.Tests;
 
 public class PaginatedResultTests
 {
-    [Fact]
-    public void Empty_collection_should_produce_no_previous_or_next_urls()
+    public class UnitTests
     {
-        var listWithoutCollection = new PaginatedResult<int>(Array.Empty<int>());
 
-        Assert.Null(listWithoutCollection.NextPage);
-        Assert.Null(listWithoutCollection.PreviousPage);
-        Assert.Empty(listWithoutCollection.Items);
+        [Fact]
+        public void Empty_collection_should_produces_no_previous_or_next_urls()
+        {
+            var listWithoutCollection = new PaginatedResult<int>(Array.Empty<int>(), 10, 10, string.Empty);
+
+            Assert.Null(listWithoutCollection.NextPage);
+            Assert.Null(listWithoutCollection.PreviousPage);
+            Assert.Empty(listWithoutCollection.Items);
+        }
+
+        [Fact]
+        public void Collection_with_ten_elements_and_page_size_of_five_produces_five_items_without_a_previous_url_and_with_a_next_url()
+        {
+            // arrange
+            const int pageSize = 5;
+            const int repeatedElement = 0;
+
+            // act
+            var result = new PaginatedResult<int>(Enumerable.Repeat(repeatedElement, 10), pageSize, 0, "http://localhost/");
+
+            // assert
+            Assert.Null(result.PreviousPage);
+            Assert.Equal($"http://localhost/?pageSize={pageSize}&page=1", result.NextPage);
+            Assert.Equal(result.Items, Enumerable.Repeat(repeatedElement, pageSize));
+
+        }
     }
 }
--- GameSync.Api/PaginatedResult.cs
+++ GameSync.Api/PaginatedResult.cs
@@ -3,15 +3,44 @@
 public class PaginatedResult<T>
 {
 
-    public PaginatedResult(IEnumerable<T> collection)
+    public PaginatedResult(IEnumerable<T> collection, int pageSize, int pageNumber, string absoluteUrlToDestination)
     {
-        Items = collection;
+        if (pageSize <= 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(pageSize));
+        }
+
+        if (pageNumber < 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(pageNumber));
+        }
+
+        int offset = pageSize * pageNumber;
+        int count = collection.Count();
+
+        if (offset + pageSize < count) // there are items still left in the next page 
+        {
+            NextPage = BuildPageUrl(pageNumber + 1, pageSize, absoluteUrlToDestination);
+
+        }
+
+        Items = collection.Skip(offset).Take(pageSize).ToList();
+
     }
 
 
     public IEnumerable<T> Items { get; set; }
     public string? NextPage { get; private set; }
     public string? PreviousPage { get; private set; }
+
+    private string BuildPageUrl(int page, int pageSize, string absoluteUrl)
+    {
+
+        var builder = new UriBuilder(absoluteUrl);
+        var begginingChar = builder.Query.Contains('?') ? "&" : "?";
+        builder.Query += $"{begginingChar}pageSize={pageSize}&page={page}";
+        return builder.Uri.AbsoluteUri;
+    }
 
 
 }

ea4e635723ad0a8dba1f49ef6ab26c6f29d3d439
Kevin Ferati <kevin.ferati@hes-so.ch>
2023-08-15 18:46:59 +0200

    PaginatedResult: Created the PaginatedResult class with null next and previous pages being null

---
+++ GameSync.Api.Tests/PaginatedResultTests.cs
@@ -0,0 +1,16 @@
+﻿using Xunit;
+
+namespace GameSync.Api.Tests;
+
+public class PaginatedResultTests
+{
+    [Fact]
+    public void Empty_collection_should_produce_no_previous_or_next_urls()
+    {
+        var listWithoutCollection = new PaginatedResult<int>(Array.Empty<int>());
+
+        Assert.Null(listWithoutCollection.NextPage);
+        Assert.Null(listWithoutCollection.PreviousPage);
+        Assert.Empty(listWithoutCollection.Items);
+    }
+}
---
+++ GameSync.Api/PaginatedResult.cs
@@ -0,0 +1,17 @@
+﻿namespace GameSync.Api;
+
+public class PaginatedResult<T>
+{
+
+    public PaginatedResult(IEnumerable<T> collection)
+    {
+        Items = collection;
+    }
+
+
+    public IEnumerable<T> Items { get; set; }
+    public string? NextPage { get; private set; }
+    public string? PreviousPage { get; private set; }
+
+
+}

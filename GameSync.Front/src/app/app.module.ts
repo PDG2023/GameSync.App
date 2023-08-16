29ea7d4a52ac142f70980a8b5d8122329e810d7c
bchollet <babacouda@gmail.com>
2023-08-16 12:27:49 +0200

    feat: :sparkles: added navigation

--- GameSync.Front/src/app/app.module.ts
+++ GameSync.Front/src/app/app.module.ts
@@ -1,22 +1,26 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AppRoutingModule } from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {AppRoutingModule} from './app-routing.module';
 import {HomeComponent} from "./features/home/home.component";
 import {MatSidenavModule} from "@angular/material/sidenav";
-import { SideNavComponent } from './common/side-nav/side-nav.component';
+import {SideNavComponent} from './common/side-nav/side-nav.component';
 import {MatListModule} from "@angular/material/list";
 import {MatToolbarModule} from "@angular/material/toolbar";
 import {MatButtonModule} from "@angular/material/button";
 import {MatIconModule} from "@angular/material/icon";
+import {CollectionComponent} from './features/collection/collection.component';
+import {PartiesComponent} from './features/parties/parties.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     SideNavComponent,
+    CollectionComponent,
+    PartiesComponent,
   ],
   imports: [
     BrowserModule,

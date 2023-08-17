1d195d5967c2edee4cfe68f6d1c560bdeacc6be0
Kevin Ferati <kevin.ferati@hes-so.ch>
2023-08-17 18:32:19 +0200

    Added an alphanumericvaluegenerator for the game id

--- GameSync.Api.Persistence/Entities/Game.cs
+++ GameSync.Api.Persistence/Entities/Game.cs
@@ -1,16 +1,21 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace GameSync.Api.Persistence.Entities;
 
 public class Game
 {
-    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.None)]
     public string? Id { get; set; } 
-    public required string Name { get; init; }
+    public required string Name { get; set; }
 
-    public required int MinPlayer { get; init; }
-    public required int MaxPlayer { get; init; }
-    public required int MinAge { get; init; }
+    public required int MinPlayer { get; set; }
+    public required int MaxPlayer { get; set; }
+    public required int MinAge { get; set; }
+
+    public int? Duration { get; set; }
+
+    public string? Description { get; set; }
 
     public required string UserId { get; init; }
     public User? User { get; set; }
---
+++ GameSync.Api.Persistence/Migrations/20230817155249_GameStringForId.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GameSync.Api.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class GameStringForId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Games",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Duration",
+                table: "Games",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "MaxPlayer",
+                table: "Games",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "MinAge",
+                table: "Games",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "MinPlayer",
+                table: "Games",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "Games",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Games_UserId",
+                table: "Games",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Games_AspNetUsers_UserId",
+                table: "Games",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Games_AspNetUsers_UserId",
+                table: "Games");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Games_UserId",
+                table: "Games");
+
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Games");
+
+            migrationBuilder.DropColumn(
+                name: "Duration",
+                table: "Games");
+
+            migrationBuilder.DropColumn(
+                name: "MaxPlayer",
+                table: "Games");
+
+            migrationBuilder.DropColumn(
+                name: "MinAge",
+                table: "Games");
+
+            migrationBuilder.DropColumn(
+                name: "MinPlayer",
+                table: "Games");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Games");
+        }
+    }
+}

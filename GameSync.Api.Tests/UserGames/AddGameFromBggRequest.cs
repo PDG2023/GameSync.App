d750eab4c30ab629bee00c79fafa8416e3643436
Kevin Ferati <kevin.ferati@hes-so.ch>
2023-08-20 09:14:45 +0200

    Created the boilerplate for adding games from BGG

---
+++ GameSync.Api.Tests/UserGames/AddGameFromBggRequest.cs
@@ -0,0 +1,38 @@
+﻿using FastEndpoints;
+using GameSync.Api.Endpoints.Users.Me.Games;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Org.BouncyCastle.Crypto;
+using System.Net;
+using Xunit;
+
+namespace GameSync.Api.Tests.UserGames;
+
+[Collection("FullApp")]
+public class AddGameFromBggRequest : TestsWithLoggedUser
+{
+    public AddGameFromBggRequest(GameSyncAppFactory factory) : base(factory)
+    {
+    }
+
+
+    [Fact]
+    public async Task Adding_non_existing_game_from_bgg_produces_not_found()
+    {
+
+        // arrange
+        const int nonExistingId = 848965651;
+        var addExistingGameRequest = new Endpoints.Users.Me.Games.AddGameFromBggRequest
+        {
+            IDs = new[] { nonExistingId }
+        };
+
+        // act
+        var (response, result) = await Client.POSTAsync<AddGameFromBggEndpoint, Endpoints.Users.Me.Games.AddGameFromBggRequest, NotFound<List<int>>>(addExistingGameRequest);
+
+        // assert
+        Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+        Assert.NotNull(result);
+        Assert.Equal(nonExistingId, Assert.Single(result.Value));
+
+    }
+}
---
+++ GameSync.Api/Endpoints/Users/Me/Games/AddGameFromBggEndpoint.cs
@@ -0,0 +1,39 @@
+﻿using Duende.IdentityServer.Events;
+using FluentValidation;
+using Microsoft.AspNetCore.Http.HttpResults;
+using System.Text.Json.Serialization;
+
+namespace GameSync.Api.Endpoints.Users.Me.Games;
+
+public class AddGameFromBggRequest
+{
+    [JsonPropertyName("ids")]
+    public required IEnumerable<int> IDs { get; init; } 
+}
+
+public class AddGameFromBggValidator : Validator<AddGameFromBggRequest>
+{
+    public AddGameFromBggValidator()
+    {
+        RuleFor(x => x.IDs).NotEmpty();
+        RuleForEach(x => x.IDs).GreaterThan(0);
+    }
+}
+
+public class AddGameFromBggEndpoint : Endpoint<AddGameFromBggRequest, Results<NotFound<List<int>>, Ok>>
+{
+
+    public override void Configure()
+    {
+        Post("from-bgg");
+        Group<CollectionGroup>();
+    }
+
+    public override Task<Results<NotFound<List<int>>, Ok>> ExecuteAsync(AddGameFromBggRequest req, CancellationToken ct)
+    {
+
+
+
+        return base.ExecuteAsync(req, ct);
+    }
+}

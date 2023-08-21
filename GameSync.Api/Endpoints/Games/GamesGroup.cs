6913b5bf67d307bc5412bf99d75b37931d8d0d37
Kevin Ferati <kevin.ferati@hes-so.ch>
2023-08-21 17:59:28 +0200

    Create a GamesGroup with a "games" prefix and updated the SearchEndpoint

---
+++ GameSync.Api/Endpoints/Games/GamesGroup.cs
@@ -0,0 +1,11 @@
+﻿namespace GameSync.Api.Endpoints.Games;
+
+public class GamesGroup : Group
+{
+    public const string Prefix = "games";
+
+    public GamesGroup()
+    {
+        Configure(Prefix, x => x.AllowAnonymous());
+    }
+}
---
+++ GameSync.Api/Endpoints/Games/GetGameEndpoint.cs
@@ -0,0 +1,18 @@
+﻿using GameSync.Api.Persistence.Entities;
+using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace GameSync.Api.Endpoints.Games;
+
+public class GetGameRequest
+{
+    public required int Id { get; set; }
+}
+
+public class GetGameEndpoint : Endpoint<GetGameRequest, Results<Ok<Game>, NotFound>>
+{
+    public override void Configure()
+    {
+
+        Group<GamesGroup>();
+    }
+}

f7165f6006d376d75b5c44b810bc0d670c5cc29a
bchollet <babacouda@gmail.com>
2023-08-22 23:16:54 +0200

    feat: :lipstick: added collection layout

--- GameSync.Front/src/app/models/models.ts
+++ GameSync.Front/src/app/models/models.ts
@@ -9,3 +9,20 @@ export interface HttpErrorResponseDetail {
   code: number;
   description: string;
 }
+
+export interface Game {
+  name: string;
+  minPlayer: number;
+  maxPlayer: number;
+  minAge: number;
+  description: string;
+  durationMinute: number;
+}
+
+export interface GameDetail extends Game {
+  id: number;
+}
+
+export interface GameList extends GameDetail {
+  userId: string;
+}

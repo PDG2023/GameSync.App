d5b85f7497a2f57ccd14fd1ec47dd6acac1dfb9d
Kevin Ferati <kevin.ferati@hes-so.ch>
2023-09-06 13:38:22 +0200

    [skip ci] personal data attributes for parties

--- GameSync.Api.Persistence/Entities/Party.cs
+++ GameSync.Api.Persistence/Entities/Party.cs
@@ -1,4 +1,5 @@
 ﻿
+using Microsoft.AspNetCore.Identity;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace GameSync.Api.Persistence.Entities;
@@ -6,8 +7,12 @@ namespace GameSync.Api.Persistence.Entities;
 public class Party
 {
     public int Id { get; set; }
+    [ProtectedPersonalData]
     public string? Location { get; set; }
+
+    [ProtectedPersonalData]
     public string Name { get; set; }
+    [ProtectedPersonalData]
     public DateTime DateTime { get; set; }
     public string UserId { get; set; }
 

eb2a951df5b0feefed8763f84f5c0f6f59fe5b83
Kevin Ferati <kevin.ferati@hes-so.ch>
2023-09-06 08:38:18 +0200

    Call to /me when the app is launched

--- GameSync.Front/src/app/app.component.ts
+++ GameSync.Front/src/app/app.component.ts
@@ -1,4 +1,7 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {LoginService} from "./services/login.service";
+import {AuthService} from "./services/auth.service";
+import {tap} from "rxjs";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,17 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit{
+
+  constructor(
+    private loginService: LoginService,
+    private authService: AuthService
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.loginService
+      .getMe()
+      .subscribe(x => this.authService.setConnectedUser(x));
+  }
 }
